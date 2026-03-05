8fe8de48c8527260616be507540e2218d8a8d39a
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-05 21:19:14 +0700

    request url

---
+++ test/3.request-url.test.js
@@ -0,0 +1,25 @@
+import express from "express";
+import request from "supertest";
+
+test("Request", async () => {
+  const app = express();
+  app.get("/hello/world", (req, res) => {
+    res.json({
+      path: req.path,
+      originalUrl: req.originalUrl,
+      hostname: req.hostname,
+      protocol: req.protocol,
+    });
+  });
+
+  const response = await request(app)
+    .get("/hello/world")
+    .query({ name: "World" });
+
+  expect(response.body).toEqual({
+    path: "/hello/world",
+    originalUrl: "/hello/world?name=World",
+    hostname: "127.0.0.1",
+    protocol: "http",
+  });
+});
