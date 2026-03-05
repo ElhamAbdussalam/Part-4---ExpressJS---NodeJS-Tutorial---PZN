9da7fcdaea94364a040752b193195c36106baab3
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-06 05:21:48 +0700

    redirect

---
+++ test/10.redirect.test.js
@@ -0,0 +1,13 @@
+import express from "express";
+import request from "supertest";
+
+test("Request", async () => {
+  const app = express();
+  app.get("/", (req, res) => {
+    res.redirect("/to-next-page");
+  });
+
+  const response = await request(app).get("/");
+  expect(response.status).toBe(302);
+  expect(response.get("Location")).toBe("/to-next-page");
+});
