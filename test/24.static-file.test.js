f1f0fa4147f13d4d30183949dd70c452755e7ca3
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-08 05:56:38 +0700

    static file

---
+++ test/24.static-file.test.js
@@ -0,0 +1,15 @@
+import express from "express";
+import request from "supertest";
+
+test("Static File", async () => {
+  const app = express();
+  app.use(express.static(__dirname + "/static"));
+  app.get("/", (req, res) => {
+    res.send("Hello response");
+  });
+
+  let response = await request(app).get("/");
+  expect(response.text).toBe("Hello response");
+  response = await request(app).get("/contoh.txt");
+  expect(response.text).toContain("Hello Static");
+});
